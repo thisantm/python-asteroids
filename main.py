4583b188950f4c7af014c19caca6a11c71fec6d4
thisantm <thiago.sant.mach@gmail.com>
2024-08-19 21:02:50 -0300

    feat: Add frame rate control

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = fps.tick(60)/1000
 
 
 if __name__ == "__main__":
