cafe16de5edd2f30e2616cbb42e9365aa98263c9
thisantm <thiago.sant.mach@gmail.com>
2024-08-19 21:31:51 -0300

    feat: draw player and make it rotate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
@@ -7,13 +8,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     fps = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-        dt = fps.tick(60)/1000
+        dt = fps.tick(60) / 1000
 
 
 if __name__ == "__main__":
